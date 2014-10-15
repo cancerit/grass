dea7a47a50e03d5315be4f672a1b102845eca16e
Keiran Raine <keiranmraine@gmail.com>
2014-10-15 20:21:46 +0100

    Ensure it's possible to generate VCF with no output

--- bin/grass.pl
+++ bin/grass.pl
@@ -204,6 +204,9 @@ sub do_file {
 	my $entry_is_bedpe = do_data_line($fh_out, $line, $field, $is_refract, $list_between, $show_biotype, $genome_data);
 	if ($entry_is_bedpe) { $is_bedpe = 1; };
     }
+  if($is_bedpe != 1 && -s $infile == 0 && $infile =~ m/\.bedpe$/i) {
+	  $is_bedpe = 1;
+	}
     return($outfile, $is_bedpe);
 }
 #------------------------------------------------------------------------------------------------#
